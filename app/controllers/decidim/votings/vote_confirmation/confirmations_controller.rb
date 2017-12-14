fdb65c9b6b6fe5a5ab8036d6b2fae5277566a4f5
Jose Miguel Díez de la Lastra <jmdiez@aspgems.com>
2017-12-14 11:44:19 +0100

    operator change

--- app/controllers/decidim/votings/vote_confirmation/confirmations_controller.rb
+++ app/controllers/decidim/votings/vote_confirmation/confirmations_controller.rb
@@ -7,7 +7,7 @@ module Decidim
         def confirm
           voter_id = params[:voter_id]
           vote = Vote.where(voter_identifier: voter_id).first || SimulatedVote.where(voter_identifier: voter_id).first
-          vote.confirm! if vote && vote.voting.try(:voting_identifier) == params[:election_id]
+          vote.confirm! if vote && vote.voting&.voting_identifier == params[:election_id]
           render json: { result: "ok" }
         end
       end
