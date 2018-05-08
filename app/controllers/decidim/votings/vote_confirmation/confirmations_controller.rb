8a2068913d0aa9696940ff7958f506feffcc7621
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-08 19:30:26 -0300

    Bump decidim dependency to 0.11.0.pre1 (#47)

--- app/controllers/decidim/votings/vote_confirmation/confirmations_controller.rb
+++ app/controllers/decidim/votings/vote_confirmation/confirmations_controller.rb
@@ -6,7 +6,7 @@ module Decidim
       class ConfirmationsController < Decidim::Votings::VoteConfirmation::ApplicationController
         def confirm
           voter_id = params[:voter_id]
-          vote = Vote.where(voter_identifier: voter_id).first || SimulatedVote.where(voter_identifier: voter_id).first
+          vote = Vote.find_by(voter_identifier: voter_id) || SimulatedVote.find_by(voter_identifier: voter_id)
           vote.confirm! if vote && vote.voting&.voting_identifier == params[:election_id]
           render json: { result: "ok" }
         end
